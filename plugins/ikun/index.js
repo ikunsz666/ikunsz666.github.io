9bd56f873db56995a30540ad143f8a1f6563bcf5
ikunsz666 <124548189+ikunsz666@users.noreply.github.com>
2023-02-27 20:40:34 +0800

    Add files via upload

--- plugins/ikun/index.js
+++ plugins/ikun/index.js
@@ -1,8 +1,8 @@
 const { PupPlugin } = require('@pupbot/core')
 const plugin = new PupPlugin('xiaoheizi','1.1.4')
 const{segment}= require('oicq')
-const fs = require('fs')
-const download = require('download')
+//const fs = require('fs')
+//const download = require('download')
 
 const config = plugin.loadConfig()
 
@@ -16,7 +16,7 @@ function repeatStringNumTimes(str, num){
 }
 plugin.onMounted(()=>{
     plugin.on('message',async (event)=>{
-        if(event.message[0].type === "image" && !config.imgs.includes(event.message[0].file)){
+        /*if(event.message[0].type === "image" && !config.imgs.includes(event.message[0].file)){
             console.log(event.message[0].file)
             config.imgs.push(event.message[0].file)
             await download(event.message[0].url, 'D:/ikunBOT/img/')
@@ -42,7 +42,8 @@ plugin.onMounted(()=>{
             var progress = Math.round((imgcount/10000)*10)
             event.reply("-=坤图小助手=-\r总图数"+imgcount +"\r现已收集表情包："+config.jpg+"\r现已收集动图："+config.gif+'\r收集功能正常运行中')
             event.reply("-=万图计划=-\r"+imgcount+"/10000\r["+repeatStringNumTimes("=", progress)+repeatStringNumTimes("-",(10 - progress))+"]")
-        }else if(event.raw_message==="坤曲"){
+        }else */
+        if(event.raw_message==="坤曲"){
             event.reply("加载坤曲中，请稍等")
             event.reply(segment.record('https://ghproxy.com/https://raw.githubusercontent.com/ikunsz666/ikun-img/main/kunqu/'+Math.round(Math.random()*51)+'.mp3'))
         }
