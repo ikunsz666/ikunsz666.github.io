a521dede99807ee8bf684155c22c30ec957f6dd5
ikunsz666 <124548189+ikunsz666@users.noreply.github.com>
2023-03-25 14:10:08 +0800

    Delete plugins directory

--- plugins/agree/index.js
+++
@@ -1,8 +0,0 @@
-const { PupPlugin } = require('@pupbot/core')
-const plugin = new PupPlugin('agree','1.0.0')
-plugin.onMounted((bot)=>{
-    plugin.on("request.group.add",(event)=>{
-        bot.setGroupAddRequest()
-    })
-})
-module.exports = { plugin }
